32161db627b41d67c68ccae15b4d8b506eab743d
nrgould <nrgould197@gmail.com>
2025-03-02 13:05:46 -0500

    fixed metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,13 +18,36 @@ const josefinSans = Josefin_Sans({
 });
 
 export const metadata: Metadata = {
-	title: 'Nicholas Gordon | Photography',
+	title: 'Nicholas Gould | Photography',
 	description: 'Product and Lifestyle Photography Portfolio',
 	keywords: [
 		'photography',
 		'product photography',
 		'lifestyle photography',
 		'portfolio',
+		'nicholas gould',
+		'Raleigh, NC photographer',
+		'Raleigh, NC product photographer',
+		'Raleigh, NC lifestyle photographer',
+		'Raleigh, NC food photographer',
+		'Raleigh, NC product photography',
+		'Raleigh, NC lifestyle photography',
+		'Raleigh, NC food photography',
+		'Raleigh, NC product photography',
+		'Morganton, NC photographer',
+		'Morganton, NC product photographer',
+		'Morganton, NC lifestyle photographer',
+		'Morganton, NC food photographer',
+		'Morganton, NC product photography',
+		'Morganton, NC lifestyle photography',
+		'Morganton, NC food photography',
+		'Morganton, NC product photography',
+		'Apex, NC photographer',
+		'Apex, NC product photographer',
+		'Apex, NC lifestyle photographer',
+		'Apex, NC food photographer',
+		'Apex, NC product photography',
+		'Apex, NC lifestyle photography',
 	],
 };
 
